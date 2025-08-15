5a447392834ad82c4a53e0773d6386a415c85ea7
kar ray <tay.karray@gmail.com>
2025-08-15 14:57:43 +0800

    Spring simulation with Euler integrator

--- spring_euler.py
+++ spring_euler.py
@@ -12,24 +12,34 @@ def main():
     k = 10
     m = 0.1
     dt = 0.005
+    e0 = 0.5 * k * x0**2 + 0.5 * m * v0**2
     
     #Initialise discrete timesteps
     t = np.arange(1000)*dt 
     
-    #Intialise empty np array which will contain the values of x and v
+    #Intialise empty np array which will contain the values of x, v and e
     x = np.zeros(len(t))
     v = np.zeros(len(t))
+    e = np.zeros(len(t))
     
     x[0] = x0
     v[0] = v0
+    e[0] = e0
     
     for i in range(len(t)-1):
         x[i+1] = x[i] + dt * v[i]
         v[i+1] = v[i] - dt * k * x[i] / m
+        e[i+1] = 0.5 * k * x[i]**2 + 0.5 * m * v[i]**2
         
-    plt.plot(t, x)
-    plt.plot(t, v)
+    plt.plot(t, x, label="Position x(t)")
+    plt.plot(t, v, label="Velocity v(t)")
+    plt.plot(t, e, label="Energy E(t)")
+    plt.xlabel("Time/s")
+    plt.legend() 
     plt.show()
+    
+    #x, v and e are noted to be increasing over time
+    #This is expected as the euler method is unstable and does not conserve energy.
     
 if __name__ == "__main__":
     main()
